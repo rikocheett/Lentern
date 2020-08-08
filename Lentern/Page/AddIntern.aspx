b531fe3509a1a0df9435bc31deab7ff4f22fd960
rikocheett <01.sasha.01@mail.ru>
2020-08-08 16:37:26 +0300

    Added web-form AddIntern.aspx

---
+++ Lentern/Page/AddIntern.aspx
@@ -0,0 +1,93 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AddIntern.aspx.cs" Inherits="Lentern.Page.AddIntern" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html" />
+    <title>Lentern</title>
+    <style type="text/css">
+        html, body {
+            margin: 0;
+            padding: 0;
+        }
+
+        body {
+            background: white;
+        }
+
+        header {
+            width: 100%;
+            background: #80A6FF;
+        }
+
+        table.text {
+            width: 97%; /* Ширина таблицы */
+            border-spacing: 0; /* Расстояние между ячейками */
+            margin: auto;
+            padding: 10px;
+        }
+
+            table.text td {
+                width: 50%; /* Ширина ячеек */
+                vertical-align: top; /* Выравниaвание по верхнему краю */
+            }
+
+                td.rightcol { /* Правая ячейка */
+                    text-align: right; /* Выравнивание по правому краю */
+                }
+
+        .reg {
+            margin: auto;
+            margin-top: 50px;
+            width: 300px;
+            height: 400px;
+            border: 1px solid black;
+            background: #D7D7D7;
+        }
+
+            .reg p, input, button {
+                position: relative;
+                top: 20px;
+                left: 60px;
+            }
+
+            .reg input.DateClass1 {
+                width: 25px;
+            }
+
+            .reg input.DateClass2 {
+                width: 40px;
+            }
+    </style>
+</head>
+<body>
+    <form runat="server" method="post">
+    <header>
+        <table class="text">
+            <tr>
+                <td>Lentern</td>
+                <td runat="server" class="rightcol"><asp:textbox runat="server" type="text" ID="User">Добавление нового стажера</asp:textbox></td>
+            </tr>
+        </table>
+    </header>    
+        <div class="reg">
+            <p>Заполните все данные:</p>
+            <asp:textbox runat="server" id="Name" placeholder="ФИО*" />
+            <asp:textbox runat="server" id="UniversityName" placeholder="Название ВУЗА" />
+            <asp:textbox runat="server" id="Cours" placeholder="Курс (цифрой)" />
+            <asp:textbox runat="server" id="Faculty" placeholder="Факультет" />
+            <asp:textbox runat="server" id="Phone" placeholder="Контактный телефон*" />
+            <asp:textbox runat="server" id="Email" placeholder="Почта*" />
+            <asp:textbox runat="server" id="About" placeholder="О себе" />
+            <p>Дата рождения*:</p>
+            <asp:textbox runat="server" class="DateClass1" id="Day" placeholder="ДД" />
+            <asp:textbox runat="server" class="DateClass1" id="Month" placeholder="ММ" />
+            <asp:textbox runat="server" class="DateClass2" id="Year" placeholder="ГГГГ" />
+            <br />
+            <br />
+            <asp:button runat="server" name="AddInt" id="AddInt" text="Отправить!" OnClick="AddIntern_Click"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Lentern/Page/AddIntern.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Lentern.Model;
+
+namespace Lentern.Page
+{
+    public partial class AddIntern : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Owner own = new Owner();
+            string userurl = Request.QueryString["User"];
+            if (!String.IsNullOrEmpty(userurl))
+            {
+                Encoding enc = new Encoding();
+                string user = enc.decode(userurl);
+                User.Text = "Добавление нового стажера<br/>" + user;
+            }
+        }
+
+        protected void AddIntern_Click(object sender, EventArgs e)
+        {
+
+
+            String name = Name.Text;
+            String universityName = UniversityName.Text;
+            String cours = Cours.Text;
+            String faculty = Faculty.Text;
+            String phone = Phone.Text;
+            String email = Email.Text;
+            String about = About.Text;
+            String day = Day.Text;
+            String month = Month.Text;
+            String year = Year.Text;
+
+            if (String.IsNullOrEmpty(name) && String.IsNullOrEmpty(phone) && String.IsNullOrEmpty(email) && String.IsNullOrEmpty(day) &&
+                String.IsNullOrEmpty(month) && String.IsNullOrEmpty(year) && Int32.TryParse(cours, out int a) && Int32.TryParse(day, out int u) &&
+                Int32.TryParse(month, out int b) && Int32.TryParse(year, out int c))
+            {
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Введите все обязательные поля! Либо поля с цифровым значением введины некоректно!')", true);
+            }
+            else
+            {
+                string userurl = Request.QueryString["User"];
+                String createName;
+                if (!String.IsNullOrEmpty(userurl))
+                {
+                    Encoding enc = new Encoding();
+                    string user = enc.decode(userurl);
+                    createName = user;
+                }
+                else createName = email;
+                DateTime time = DateTime.Now;
+                DateTime birthDate = new DateTime(Int32.Parse(year), Int32.Parse(month), Int32.Parse(day));
+                using (LenternContext db = new LenternContext())
+                {
+                    Intern i = new Intern
+                    {
+                        Name = name,
+                        BirthDate = birthDate,
+                        UniversityName = universityName,
+                        Course = Int32.Parse(cours),
+                        Faculty = faculty,
+                        Phone = phone,
+                        Email = email,
+                        About = about,
+                        CreateDate = time,
+                        EditDate = time,
+                        CreateName = createName,
+                        EditName = createName
+                    };
+                    db.Interns.Add(i);
+                    db.SaveChanges();
+                    ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Запись стажера добавлена!')", true);
+                }
+            }
+        }
+    }
+}
